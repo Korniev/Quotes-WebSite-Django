1a8cfe937d51715cdc1dd8345cb8f609e647559a
Yevhenii Korniev <94477980+Korniev@users.noreply.github.com>
2023-12-07 16:27:42 +0100

    updated

--- quotes_website/quotes/views.py
+++ quotes_website/quotes/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from .utils import get_mongo_db
+from django.core.paginator import Paginator
 # Create your views here.
 
 
-def main(request):
+def main(request, page=1):
     db = get_mongo_db()
     quotes = db.quotes.find()
-    return render(request, 'quotes/index.html', context={'quotes': quotes})
+    per_page = 10
+    paginator = Paginator(list(quotes), per_page=per_page)
+    quotes_on_page = paginator.page(page)
+    return render(request, 'quotes/index.html', context={'quotes': quotes_on_page})
