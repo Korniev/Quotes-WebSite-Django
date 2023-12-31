81d1bf824ce7810ed35cccf920de8f260ffb9e6e
Yevhenii Korniev <94477980+Korniev@users.noreply.github.com>
2023-12-31 15:33:13 +0100

    updated views

--- quotes_website/users/views.py
+++ quotes_website/users/views.py
@@ -1,17 +1,22 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.views import PasswordResetView
+from django.contrib.messages.views import SuccessMessageMixin
+from django.urls import reverse_lazy
 from django.contrib.auth import login, authenticate, logout
-from .forms import SignUpForm, LoginForm
+from .forms import RegisterForm, LoginForm
 
 
 def signup_view(request):
     if request.method == 'POST':
-        form = SignUpForm(request.POST)
+        form = RegisterForm(request.POST)
         if form.is_valid():
             user = form.save()
             login(request, user)
             return redirect('quotes:root')
     else:
-        form = SignUpForm()
+        form = RegisterForm()
     return render(request, 'users/signup.html', {'form': form})
 
 
@@ -32,3 +37,12 @@ def login_view(request):
 def logout_view(request):
     logout(request)
     return redirect('quotes:root')
+
+
+class ResetPasswordView(SuccessMessageMixin, PasswordResetView):
+    template_name = 'users/password_reset.html'
+    email_template_name = 'users/password_reset_email.html'
+    html_email_template_name = 'users/password_reset_email.html'
+    success_url = reverse_lazy('users:password_reset_done')
+    success_message = "An email with instructions to reset your password has been sent to %(email)s."
+    subject_template_name = 'users/password_reset_subject.txt'
